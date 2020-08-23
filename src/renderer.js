73406dcadda424678b4bcf1b4ca11bde3561a2f3
rtaha07 <rtaha07@gmail.com>
2020-08-23 15:55:43 -0400

    commit

--- src/renderer.js
+++ src/renderer.js
@@ -6,29 +6,26 @@ const init = async () => {
   render(friends);
 };
 
-render(){
-
-  const ul = document.querySelector('ul');
-  ul.addEventListener('click', async (ev) => {
-    if (ev.target.tagName === 'BUTTON') {
-      const buttonList = ev.target.classList.contains('friend-name');
-      const id = ev.target.getAttribute('data-id');
-      if (buttonList === 'add-friend') {
-        await axios.put(`api/friends/${id}`, {
-          category: 'adding',
-        });
-      } else if (buttonList === 'subtract-friend') {
-        await axios.put(`api/friends/${id}`, {
-          category: 'subtracting',
-        });
-      } else if (buttonList === 'clear-friend') {
-        await axios.delete(`api/friends/${id}`, {
-          category: 'deleting',
-        });
-      }
+const ul = document.querySelector('ul');
+ul.addEventListener('click', async (ev) => {
+  if (ev.target.tagName === 'BUTTON') {
+    const buttonList = ev.target.classList.contains('friend-name');
+    const id = ev.target.getAttribute('data-id');
+    if (buttonList === 'add-friend') {
+      await axios.put(`api/friends/${id}`, {
+        category: 'adding',
+      });
+    } else if (buttonList === 'subtract-friend') {
+      await axios.put(`api/friends/${id}`, {
+        category: 'subtracting',
+      });
+    } else if (buttonList === 'clear-friend') {
+      await axios.delete(`api/friends/${id}`, {
+        category: 'deleting',
+      });
     }
-  });
-}
+  }
+});
 
 init();
 
