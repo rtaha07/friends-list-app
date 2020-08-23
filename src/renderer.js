57324492240137f9ef50c7f12cbaa8561abfb98f
rtaha07 <rtaha07@gmail.com>
2020-08-23 16:05:01 -0400

    commit

--- src/renderer.js
+++ src/renderer.js
@@ -6,28 +6,30 @@ const init = async () => {
   render(friends);
 };
 
-const ul = document.querySelector('ul');
-ul.addEventListener('click', async (ev) => {
-  if (ev.target.tagName === 'BUTTON') {
-    const buttonList = ev.target.classList.contains('friend-name');
-    const id = ev.target.getAttribute('data-id');
-    if (buttonList === 'add-friend') {
-      await axios.put(`api/friends/${id}`, {
-        category: 'adding',
-      });
-    } else if (buttonList === 'subtract-friend') {
-      await axios.put(`api/friends/${id}`, {
-        category: 'subtracting',
-      });
-    } else if (buttonList === 'clear-friend') {
-      await axios.delete(`api/friends/${id}`, {
-        category: 'deleting',
-      });
-    }
-  }
-});
-
 init();
+
+const render () => {
+  const ul = document.querySelector('ul');
+  ul.addEventListener('click', async (ev) => {
+    if (ev.target.tagName === 'BUTTON') {
+      const buttonList = ev.target.classList.contains('friend-name');
+      const id = ev.target.getAttribute('data-id');
+      if (buttonList === 'add-friend') {
+        await axios.put(`api/friends/${id}`, {
+          category: 'adding',
+        });
+      } else if (buttonList === 'subtract-friend') {
+        await axios.put(`api/friends/${id}`, {
+          category: 'subtracting',
+        });
+      } else if (buttonList === 'clear-friend') {
+        await axios.delete(`api/friends/${id}`, {
+          category: 'deleting',
+        });
+      }
+    }
+  });
+}
 
 module.exports = {
   render,
